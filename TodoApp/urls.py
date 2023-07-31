04a32ca7f800f54f8fed6c2e57002670278a7339
Jarv1er <asediador@hotmail.com>
2023-07-31 18:07:55 +0200

    Creando models, views, urls y html

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import ListaTareas, DetallesTareas, CrearTarea
 
 urlpatterns = [
-    path('', views.listaTareas, name='tareas')
+    path('', ListaTareas.as_view(), name='tareas'),
+    path('tarea/<int:pk>/', DetallesTareas.as_view(), name='tarea'),
+    path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Tarea
 
 # Create your views here.
 
 
-def listaTareas(request):
-    return HttpResponse("Todo List App")
+class ListaTareas(ListView):
+    model = Tarea
+    context_object_name = "tareas"
+    template_name = 'TodoApp/lista_tareas.html'
+
+
+class DetallesTareas(DetailView):
+    model = Tarea
+    context_object_name = "tarea"
+    template_name = 'TodoApp/tarea.html'
+
+
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy("tareas")
