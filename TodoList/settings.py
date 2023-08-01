d056c3020d83d94a70fa61ac89cda6aa6dfadbcb
Jarv1er <asediador@hotmail.com>
2023-08-01 22:54:44 +0200

    Problema solucionado con la carpeta static css

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +118,7 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
