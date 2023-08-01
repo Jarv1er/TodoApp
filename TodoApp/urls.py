8b46d2336e2857ca57eee8d0ed0207dad2ec3ad5
Jarv1er <asediador@hotmail.com>
2023-08-01 11:30:50 +0200

    Funcionalidad crud terminada

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import ListaTareas, DetallesTareas, CrearTarea
+from .views import ListaTareas, DetallesTarea, CrearTarea, EditarTarea, EliminarTarea
 
 urlpatterns = [
     path('', ListaTareas.as_view(), name='tareas'),
-    path('tarea/<int:pk>/', DetallesTareas.as_view(), name='tarea'),
+    path('tarea/<int:pk>/', DetallesTarea.as_view(), name='tarea'),
     path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
+    path('edi/<int:pk>/', EditarTarea.as_view(), name='editar-tarea'),
+    path('elitar-tareaminar-tarea/<int:pk>/', EliminarTarea.as_view(), name='eliminar-tarea'),
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Tarea
 
@@ -14,7 +14,7 @@ class ListaTareas(ListView):
     template_name = 'TodoApp/lista_tareas.html'
 
 
-class DetallesTareas(DetailView):
+class DetallesTarea(DetailView):
     model = Tarea
     context_object_name = "tarea"
     template_name = 'TodoApp/tarea.html'
@@ -23,4 +23,17 @@ class DetallesTareas(DetailView):
 class CrearTarea(CreateView):
     model = Tarea
     fields = '__all__'
+    success_url = reverse_lazy("tareas")
+
+
+class EditarTarea(UpdateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy("tareas")
+
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = "tareas"
+    template_name = 'TodoApp/elimnar_tarea.html'
     success_url = reverse_lazy("tareas")
