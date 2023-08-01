2b06887b2143357b4cfa2ebd0ad4c59f7a8c8d3a
Jarv1er <asediador@hotmail.com>
2023-08-01 13:46:47 +0200

    Creacion del sistema de registro terminado

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-from .views import ListaTareas, DetallesTarea, CrearTarea, EditarTarea, EliminarTarea
+from .views import ListaTareas, DetallesTarea, CrearTarea, EditarTarea, EliminarTarea, IniciarSesion
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+    path('login/', IniciarSesion.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', ListaTareas.as_view(), name='tareas'),
     path('tarea/<int:pk>/', DetallesTarea.as_view(), name='tarea'),
     path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
-    path('edi/<int:pk>/', EditarTarea.as_view(), name='editar-tarea'),
-    path('elitar-tareaminar-tarea/<int:pk>/', EliminarTarea.as_view(), name='eliminar-tarea'),
+    path('editar-tarea/<int:pk>/', EditarTarea.as_view(), name='editar-tarea'),
+    path('eliminar-tarea/<int:pk>/', EliminarTarea.as_view(), name='eliminar-tarea'),
 ]
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -3,36 +3,45 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Tarea
 
 # Create your views here.
 
+class IniciarSesion(LoginView):
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy("tareas")
 
-class ListaTareas(ListView):
+
+class ListaTareas(LoginRequiredMixin, ListView):
     model = Tarea
     context_object_name = "tareas"
     template_name = 'TodoApp/lista_tareas.html'
 
 
-class DetallesTarea(DetailView):
+class DetallesTarea(LoginRequiredMixin, DetailView):
     model = Tarea
     context_object_name = "tarea"
     template_name = 'TodoApp/tarea.html'
 
 
-class CrearTarea(CreateView):
+class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
     fields = '__all__'
     success_url = reverse_lazy("tareas")
 
 
-class EditarTarea(UpdateView):
+class EditarTarea(LoginRequiredMixin, UpdateView):
     model = Tarea
     fields = '__all__'
     success_url = reverse_lazy("tareas")
 
 
-class EliminarTarea(DeleteView):
+class EliminarTarea(LoginRequiredMixin, DeleteView):
     model = Tarea
     context_object_name = "tareas"
     template_name = 'TodoApp/elimnar_tarea.html'
