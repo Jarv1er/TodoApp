5f9ec57067e2fea3ab378c982c1beff6bfab2dca
Jarv1er <asediador@hotmail.com>
2023-08-01 18:12:54 +0200

    Creando Buscador

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views
-from .views import ListaTareas, DetallesTarea, CrearTarea, EditarTarea, EliminarTarea, IniciarSesion
+from .views import ListaTareas, DetallesTarea, CrearTarea, EditarTarea, EliminarTarea, IniciarSesion, PaginaRegistro
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('login/', IniciarSesion.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('registro/', PaginaRegistro.as_view(), name='registro'),
+
     path('', ListaTareas.as_view(), name='tareas'),
     path('tarea/<int:pk>/', DetallesTarea.as_view(), name='tarea'),
     path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
--- TodoApp/views.py
+++ TodoApp/views.py
@@ -1,23 +1,45 @@
-from typing import Any, Dict
 from django.forms.models import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Tarea
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
+
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Tarea
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
 
 # Create your views here.
 
 class IniciarSesion(LoginView):
+    template_name = "TodoApp/login.html"
     fields = '__all__'
     redirect_authenticated_user = True
     
     def get_success_url(self):
         return reverse_lazy("tareas")
+    
+
+class PaginaRegistro(FormView):
+    template_name = "TodoApp/registro.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("tareas")
+
+    def form_valid(self, form):
+        usuario = form.save()
+        if usuario is not None:
+            login(self.request, usuario)
+        return super(PaginaRegistro, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            redirect("tareas")
+        return super(PaginaRegistro, self).get(*args, **kwargs)
 
 
 class ListaTareas(LoginRequiredMixin, ListView):
@@ -29,9 +51,10 @@ class ListaTareas(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tareas'] = context['tareas'].filter(usuario=self.request.user)
         context['count'] = context['tareas'].filter(completado=False).count()
+
+        buscador = self.request.GET.get('buscador')
         return context
     
-
 
 class DetallesTarea(LoginRequiredMixin, DetailView):
     model = Tarea
@@ -45,7 +68,8 @@ class CrearTarea(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy("tareas")
 
     def form_valid(self, form):
-        return super(self, CrearTarea).form_valid(form)
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
 
 
 class EditarTarea(LoginRequiredMixin, UpdateView):
